90d194b52717b0c1731d9024e8e85aef696f0574
Gra <yvesb@live.be>
2022-01-24 23:18:44 +0100

    Added database and setup entity

--- AstolfoBot/src/index.ts
+++ AstolfoBot/src/index.ts
@@ -1,13 +1,28 @@
 require('dotenv').config();
+import 'reflect-metadata';
 import { registerCommands, registerEvents } from './utils/registry';
 import config from '../slappey.json';
 import DiscordClient from './client/client';
 import { Intents } from 'discord.js';
-const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+import { createConnection } from 'typeorm';
+import { GuildConfiguration } from './typeOrm/entities/GuildConfiguration';
+const client = new DiscordClient({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
 (async () => {
   client.prefix = config.prefix || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.BOT_TOKEN);
+  await createConnection({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    entities: [GuildConfiguration]
+  })
 })();
---
+++ AstolfoBot/src/typeOrm/entities/GuildConfiguration.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'guild_configuration' })
+export class GuildConfiguration {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column({ unique: true, name: 'guild_id' })
+    guildId: string;
+
+    @Column({ default: ',' })
+    prefix: string;
+
+    @Column({ name: 'welcome_channel_id' })
+    welcomeChannelId: string;
+}
