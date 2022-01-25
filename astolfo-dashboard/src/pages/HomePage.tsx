7e0ac6e9b6d75402f46df2d8e41e23f082252dc0
Gra <yvesb@live.be>
2022-01-25 12:50:39 +0100

    Login Page

--- astolfo-dashboard/src/pages/HomePage.tsx
+++ astolfo-dashboard/src/pages/HomePage.tsx
@@ -1,1 +1,31 @@
-export const HomePage = () => <div>HomePage</div>;
+import { FaDiscord, FaQuestionCircle } from "react-icons/fa";
+import { HomePageStyle, MainButton } from "../utils/styles";
+
+export const HomePage = () => {
+  return (
+    <HomePageStyle>
+      <div></div>
+      <div>
+        <MainButton>
+          <FaDiscord size={45} color="5865f2" />
+          <p style={{ fontSize: "18px" }}>Login with Discord</p>
+        </MainButton>
+        <MainButton>
+          <FaQuestionCircle size={41} />
+          <p style={{ fontSize: "18px" }}>Support Server</p>
+        </MainButton>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "450px",
+          justifyContent: "space-between",
+        }}
+      >
+        <span>Privacy Policy</span>
+        <span>Terms of Service</span>
+        <span>Contact Us</span>
+      </div>
+    </HomePageStyle>  
+  );
+};
