8bf84685a41056a49644f5bd84c0fac715cc14f9
Gra <yvesb@live.be>
2022-01-25 23:23:54 +0100

    [dashboard] Protecting dashboard routes

--- astolfo-dashboard/src/App.tsx
+++ astolfo-dashboard/src/App.tsx
@@ -7,24 +7,34 @@ import { LoginPage } from "./pages/LoginPage";
 import { MenuPage } from "./pages/MenuPage";
 import { WelcomeMessagePage } from "./pages/WelcomeMessagePage";
 import { GuildContext } from "./utils/contexts/GuildContext";
+import { useFetchUser } from "./utils/contexts/hooks/useFetchUser";
 
 function App() {
   const [guildId, setGuildId] = useState("");
+  const { user, loading, error } = useFetchUser();
   const updateGuildId = (id: string) => setGuildId(id);
 
   return (
     <GuildContext.Provider value={{ guildId, updateGuildId }}>
-      <Routes>
-        <Route path='/dashboard/*' element={<AppBar/>}/>
-      </Routes>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        {/* <Route path="/dashboard" element={<HomePage />} /> */}
-        <Route path="/dashboard/categories" element={<CategoryPage />} />
-        <Route path="/dashboard/prefix" element={<GuildPrefixPage />} />
-        <Route path="/dashboard/message" element={<WelcomeMessagePage />} />
-      </Routes>
+      {user ? (
+        <>
+          <Routes>
+            <Route path="/dashboard/*" element={<AppBar />} />
+          </Routes>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/menu" element={<MenuPage />} />
+            {/* <Route path="/dashboard" element={<HomePage />} /> */}
+            <Route path="/dashboard/categories" element={<CategoryPage />} />
+            <Route path="/dashboard/prefix" element={<GuildPrefixPage />} />
+            <Route path="/dashboard/message" element={<WelcomeMessagePage />} />
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      )}
     </GuildContext.Provider>
   );
 }
---
+++ astolfo-dashboard/src/utils/api.ts
@@ -0,0 +1,7 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { User } from "./types";
+
+const CONFIG: AxiosRequestConfig = { withCredentials: true };
+
+export const getAuthStatus = () =>
+    axios.get<User>('http://localhost:3001/api/auth/status', CONFIG);
