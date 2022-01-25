0ed0a4246a252a0536c0bf92d00a2516073c074e
Gra <yvesb@live.be>
2022-01-25 16:44:19 +0100

    Implement Buttons in prefix

--- astolfo-dashboard/src/pages/GuildPrefixPage.tsx
+++ astolfo-dashboard/src/pages/GuildPrefixPage.tsx
@@ -1,16 +1,28 @@
-import { Container, InputField, Title } from "../utils/styles";
+import { Button, Container, Flex, InputField, Title } from "../utils/styles";
 
 export const GuildPrefixPage = () => {
   return (
     <div style={{ padding: "35px" }}>
       <Container style={{ width: "50%" }}>
         <Title>Update Command Prefix</Title>
-        <div>
+        <form>
           <div>
             <label htmlFor="prefix">Current Prefix</label>
           </div>
-          <InputField style={{margin: '10px 0'}} />
-        </div>
+          <InputField style={{ margin: "10px 0" }} />
+          <Flex justifyContent="flex-end">
+            <Button
+              variant="secondary"
+              type="button"
+              style={{
+                marginRight: "8px",
+              }}
+            >
+              Reset
+            </Button>
+            <Button variant="primary">Save</Button>
+          </Flex>
+        </form>
       </Container>
     </div>
   );
