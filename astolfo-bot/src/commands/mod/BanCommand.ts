fb855561cff09c8f3e57e76aed4bbbfbdc1492c7
Gra <yvesb@live.be>
2022-01-27 16:58:37 +0100

    [discord-bot] Ban Command

---
+++ astolfo-bot/src/commands/mod/BanCommand.ts
@@ -0,0 +1,33 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+import { getRepository, Repository } from 'typeorm';
+import { GuildBanLog } from '../../typeOrm/entities/GuildBanLog';
+
+export default class BanCommand extends BaseCommand {
+  constructor(
+    private readonly BanLogReposity: Repository<GuildBanLog> = getRepository(GuildBanLog)
+  ) {
+    super('ban', 'mod', []);
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    const [memberId, ...rest] = args;
+    const reason = rest.join(' ');
+    try {
+      const member = await message.guild?.members.fetch(memberId)!;
+      await member.ban({ reason });
+      const guildBan = this.BanLogReposity.create({
+        guildId: message.guildId!,
+        bannedMemberId: memberId,
+        issuedBy: message.author.id,
+        reason,
+        issuedOn: new Date,
+      });
+
+      await this.BanLogReposity.save(guildBan);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
