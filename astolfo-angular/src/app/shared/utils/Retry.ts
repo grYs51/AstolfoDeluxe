a7fa1026abd241d2136a66c00d9785737d94d128
Gra <yvesb@live.be>
2022-02-04 13:04:54 +0100

    [angular] Changed RetryService

--- astolfo-angular/src/app/shared/utils/Retry.ts
+++ astolfo-angular/src/app/shared/utils/Retry.ts
@@ -9,12 +9,15 @@ export class RetryService {
   constructor(private http: HttpClient) {}
 
   async fetchData<T>(url: string, retries = 3, timeout = 0): Promise<T> {
+    if (timeout !== 0 && retries > 0) {
+      await firstValueFrom(timer(timeout).pipe(take(1)));
+    }
     if (retries > 0) {
       return firstValueFrom(
         this.http.get<T>(url, { observe: 'response', withCredentials: true })
       )
         .then((res: HttpResponse<T>) => res.body!)
-        .catch(() => this.fetchData(url, retries - 1, 1000));
+        .catch(() => this.fetchData(url, retries - 1, 300));
     } else {
       throw new Error('Something went wrong with getting data');
     }
