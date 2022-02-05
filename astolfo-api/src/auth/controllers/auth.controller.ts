1d47a3a0a11e3a9b8693a15c0edf3f700e471fd9
Gra <yvesb@live.be>
2022-02-05 23:50:01 +0100

    [backend] Changed Redirect

--- astolfo-api/src/auth/controllers/auth.controller.ts
+++ astolfo-api/src/auth/controllers/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
   @Get('redirect')
   @UseGuards(DiscordAuthGuard)
   redirect(@Res() res: Response) {
-    res.redirect('http://localhost:3000');
+    res.redirect('http://localhost:4200');
   }
 
   @Get('status')
