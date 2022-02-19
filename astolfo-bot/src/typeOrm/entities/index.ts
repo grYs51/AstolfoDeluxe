4fb971ce799ac533424a61738f6704b524759b4b
Gra <yvesb@live.be>
2022-02-19 22:45:47 +0100

    [discord-bot] removed Modarations

--- astolfo-bot/src/typeOrm/entities/index.ts
+++ astolfo-bot/src/typeOrm/entities/index.ts
@@ -3,12 +3,10 @@ import { GuildConfiguration } from './GuildConfiguration';
 import { GuildInfo } from './GuildInfo';
 import { GuildStatsLog } from './GuildsStatsLog';
 import { GuildMemberInfo } from './GuildMemberInfo';
-import { ModerationLog } from './ModerationLog';
 import { UserInfo } from './UserInfo';
 
 export const entities = [
   GuildConfiguration,
-  ModerationLog,
   GuildStatsLog,
   GuildInfo,
   ChannelInfo,
