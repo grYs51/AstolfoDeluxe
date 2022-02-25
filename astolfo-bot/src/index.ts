df2f64b67699071306a6b916d329f8fa3495d641
Gra <yvesb@live.be>
2022-02-25 20:51:45 +0100

    [discord-bot] fixed an issue with the port

--- astolfo-bot/src/index.ts
+++ astolfo-bot/src/index.ts
@@ -28,7 +28,7 @@ const client = new DiscordClient({
   await createConnection({
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: 5432,
+    port: parseInt(process.env.DB_PORT! ,10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
