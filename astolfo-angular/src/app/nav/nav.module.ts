c66e6e99d67d2e04cf8b809461b176fbd7dfa132
Gra <yvesb@live.be>
2022-02-27 12:01:59 +0100

    [angular] removed angular

--- astolfo-angular/src/app/nav/nav.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavRoutingModule } from './nav-routing.module';
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, NavRoutingModule],
-  providers: [],
-})
-export class DashboardModule {}
--- astolfo-angular/src/app/shared/services/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { IDiscordUser } from '../Types';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class UserService {
-  user: IDiscordUser | null = null;
-}
