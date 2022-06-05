ac8c07cebf05334a85485648a38bb088acd0af02
Gra <yvesb@live.be>
2022-06-05 20:09:23 +0200

    [discord-bot] small updates

--- astolfo-bot/src/index.ts
+++ astolfo-bot/src/index.ts
@@ -32,7 +32,7 @@ const client = new DiscordClient({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: false,
+    synchronize: true,
     entities: entities,
   });
 
